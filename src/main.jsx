02f65387f0631be689fbf7c8b8d38fa2ede692ce
mradul kumar katiyar <karelaking277@gmai.com>
2024-10-04 01:56:20 +0530

    feat: Integrate Redux and React Router in React app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,78 @@
 import { StrictMode } from 'react';
+import store from './backend/store';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import {
+  Home,
+  About,
+  Experience,
+  Projects,
+  Skills,
+  Contacts,
+  Login,
+  SignIn,
+} from '../src/pages/Pages';
+import AuthLayout from './components/authLayout/AuthLayout';
 import App from './pages/App';
 import './index.css';
 
+// create routes for components.
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/experience',
+        element: <Experience />,
+      },
+      {
+        path: '/projects',
+        element: <Projects />,
+      },
+      {
+        path: '/skills',
+        element: <Skills />,
+      },
+      {
+        path: '/contacts',
+        element: <Contacts />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/signin',
+        element: (
+          <AuthLayout authentication={false}>
+            <SignIn />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
+
+// Initialize React App
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
