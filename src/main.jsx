8830bca29e0e30b1e68730f20c242f39ce2fa737
mradul kumar katiyar <karelaking277@gmai.com>
2024-10-18 21:02:07 +0530

    refactor: Simplify routing and centralize definitions

--- src/main.jsx
+++ src/main.jsx
@@ -2,71 +2,9 @@ import { StrictMode } from 'react';
 import store from './backend/store';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import {
-  Home,
-  About,
-  Experience,
-  Projects,
-  Skills,
-  Contacts,
-  Login,
-  SignIn,
-} from '../src/pages/Pages';
-import AuthLayout from './components/authLayout/AuthLayout';
-import App from './pages/App';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/routes';
 import './index.css';
-
-// create routes for components.
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/experience',
-        element: <Experience />,
-      },
-      {
-        path: '/projects',
-        element: <Projects />,
-      },
-      {
-        path: '/skills',
-        element: <Skills />,
-      },
-      {
-        path: '/contacts',
-        element: <Contacts />,
-      },
-      {
-        path: '/login',
-        element: (
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: '/signin',
-        element: (
-          <AuthLayout authentication={false}>
-            <SignIn />
-          </AuthLayout>
-        ),
-      },
-    ],
-  },
-]);
 
 // Initialize React App
 createRoot(document.getElementById('root')).render(
