74e85253e3c877eb70ec701bb4b768f6fb1e4f26
White Angel <87556435+Karelaking@users.noreply.github.com>
2025-01-19 09:06:24 +0530

    update Home component styles for improved typography and layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,18 +6,21 @@ const Home = () => {
     <section
       className={`bg-white w-screen h-screen flex justify-center items-center flex-col`}
     >
-      <h2 className={`text-black text-xl`} data-aos="fade-down">
+      <h2
+        className={`text-black uppercase font-roboto text-xl sm:text-3xl`}
+        data-aos="fade-down"
+      >
         My Portfolio
       </h2>
       <h1
-        className={`uppercase px-4 overflow-hidden text-4xl sm:text-7xl text-center mt-3`}
+        className={`uppercase tracking-[0.02em] leading-16 font-monoton px-4 overflow-hidden text-5xl sm:text-7xl text-center mt-3 sm:mt-6`}
         data-aos="fade-up"
         data-aos-delay="200"
       >
         mradul kuamr katiyar
       </h1>
       <p
-        className={`w-4/5 text-black text-center text-base sm:text-2xl mt-3`}
+        className={`w-4/5 leading-6 text-black font-roboto text-center text-base sm:text-2xl mt-3 sm:mt-5`}
         data-aos="fade-in"
         data-aos-delay="400"
       >
