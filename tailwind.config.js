f585c5b5b2229b83db5b81bd01ee42b3d3ad1124
White Angel <87556435+Karelaking@users.noreply.github.com>
2025-01-19 09:06:32 +0530

    add custom font families to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
+    fontFamily: {
+      "neue": ["Bebas Neue", "serif"],
+      "monoton": ["Monoton", "serif"],
+      "roboto": ["Roboto Mono", "serif"]
+    },
     extend: {},
   },
   plugins: [],
